0fbf6875b9002022ba40472a0c5fb4846b3be5f6
Dante Lacey-Thompson <dlthompson@customink.com>
2021-03-29 13:39:18 -0400

    Home page not finished with a bit of user generation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/welcome'
+  get 'users/new'
+  get 'users/create'
   root 'static_pages#home'
-  # get 'static_pages/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+  get '/signup',  to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/welcome', to: 'sessions#welcome'
 end
