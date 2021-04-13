cc5e9bdf9c0686b96c1214a27ec6fe14aca18234
Dante Lacey-Thompson <dlthompson@customink.com>
2021-04-13 15:51:23 -0400

    Updating category test cases

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :expenses
+
+  validates_presence_of :name
+  validates_presence_of :description
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @test_category = Category.new(
+      name: 'Test Category',
+      description: 'Random detail description'
+    )
+  end
+
+  test "category should not be valid without name" do
+    @test_category.name = nil
+    assert @test_category.invalid?
+  end
+
+  test "category should not be valid without description" do
+    @test_category.description = nil
+    assert @test_category.invalid?
+  end
+
+  test "category should be valid with all details" do
+    assert @test_category.valid?
+  end
 end
